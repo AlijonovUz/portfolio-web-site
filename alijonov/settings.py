212d561ddf67215b99af6e37e45e065c5eab9827
Alijonov Abdulbosit <alijonov.me@gmail.com>
2025-03-29 14:19:01 +0500

    Update settings.py

--- alijonov/settings.py
+++ alijonov/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8(+z&^=_5(j)16y05*ne+#*k#3nql1jf92)1aa31(e6!@8f#@$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
